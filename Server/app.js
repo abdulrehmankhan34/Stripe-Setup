76f7f3627ba431dd5632b6469decb3f2ac330926
abdulrehmankhan34 <abdulrehmankhan7344@gmail.com>
2026-08-11 16:35:45 +0500

    Add Stripe webhook payment handling

--- Server/app.js
+++ Server/app.js
@@ -10,6 +10,22 @@ const app = express();
 
 // JSON data ko read karne ke liye
 app.use(cors());
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (req, res) => {
+
+    console.log("Webhook request received");
+
+    const event = JSON.parse(req.body.toString());
+
+    console.log("Event type:", event.type);
+    if (event.type === "checkout.session.completed") {
+      console.log("Payment successfully completed!");
+    }
+    res.sendStatus(200);
+  }
+);
 app.use(express.json());
 
 // Stripe object create
@@ -28,32 +44,34 @@ app.get("/stripe-test", (req, res) => {
   });
 });
 app.post("/create-checkout-session", async (req, res) => {
-  try{
-  const session = await stripe.checkout.sessions.create({
-    mode:"payment",
-    line_items: [
-    {
-      price_data: {
-        currency: "usd",
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: "payment",
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
 
-        product_data: {
-          name: "Nike Shoes",
+            product_data: {
+              name: "Nike Shoes",
+            },
+
+            unit_amount: 10000, // $100 in cents
+          },
+
+          quantity: 5,
         },
+      ],
+      success_url: "http://localhost:5173/success",
 
-        unit_amount: 10000, // $100 in cents
-      },
-
-      quantity: 5,
-    },
-  ],
-     success_url: "http://localhost:5173/success",
-
-    cancel_url: "http://localhost:5173/cancel",
-  });
- res.json({
- url: session.url});
-}catch (error) {
-  console.error("Error creating checkout session:", error);
-}});
+      cancel_url: "http://localhost:5173/cancel",
+    });
+    res.json({
+      url: session.url
+    });
+  } catch (error) {
+    console.error("Error creating checkout session:", error);
+  }
+});
 
 export default app;
